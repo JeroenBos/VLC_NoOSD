9475c04b9967538fc8fa717947319f0ee9b7e8bf
Jeroen <jer123bos@gmail.com>
2018-07-29 12:36:38 +0200

    Add project files.

---
+++ myVLC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(withNoOSDArg(args));
+        Process.Start(new ProcessStartInfo(@"C:\Program Files (x86)\VideoLAN\VLC\original vlc.exe", withNoOSDArg(args)));
+#if DEBUG
+        Console.ReadLine();
+#endif
+    }
+
+    static string withNoOSDArg(string[] args)
+    {
+        return "--no-osd " + string.Join(" ", args.Select(arg => "\"" + arg + "\""));
+    }
+}
